91f349891efcad680d677e42c2ab1e02abd6d6f6
grace-mao23 <gmao00@stuy.edu>
2019-03-05 20:16:39 -0500

    Starting 2D array now scanned in

--- USACO.java
+++ USACO.java
@@ -8,6 +8,7 @@ public class USACO {
       File f = new File(filename);
       Scanner in = new Scanner(f);
       String first = in.nextLine();
+      // think about condensing later
       int r = Integer.parseInt(first.substring(0,first.indexOf(" ")));
       first = first.substring(first.indexOf(" ")+1);
       int c = Integer.parseInt(first.substring(0,first.indexOf(" ")));
@@ -15,7 +16,13 @@ public class USACO {
       int e = Integer.parseInt(first.substring(0,first.indexOf(" ")));
       first = first.substring(first.indexOf(" ")+1);
       int n = Integer.parseInt(first);
-      System.out.println(r+","+c+","+e+","+n);
+    //  System.out.println(r+","+c+","+e+","+n);
+      int[][] board = new int[r][c];
+      for (int i = 0; i < r; i++) {
+        for (int x = 0; x < c; x++) {
+          board[i][x] = in.nextInt();
+        }
+      }
     } catch (FileNotFoundException e) {
       System.out.println("File not found");
     }
